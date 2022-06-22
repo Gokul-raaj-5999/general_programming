365bd00b960571d8a8b09c20a3c74e0b67df65e9
Gokul raaj Velu <67196614+Gokul-raaj-5999@users.noreply.github.com>
2022-06-22 11:16:21 +0530

    Update 230B - T-primes.py

--- codeforce/230B - T-primes.py
+++ codeforce/230B - T-primes.py
@@ -1,3 +1,130 @@
+import math,sys
+import os, sys
+from io import BytesIO, IOBase
+ 
+ 
+class FastIO(IOBase):
+    newlines = 0
+ 
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+ 
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, 8192))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, 8192))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+ 
+ 
+ 
+ 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+ 
+ 
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+ 
+def callone(num):
+    def prime(n):
+        prime_flag = 0
+        if(n > 2):
+            for i in range(2, int(math.sqrt(n)) + 1):
+                if (n % i == 0):
+                    prime_flag = 1
+                    break
+            if (prime_flag == 0):
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+        
+    if(num > 4):
+        x = math.sqrt(num)
+        s = str(x)
+        if(s[-2:] == '.0'):
+            if(prime(x)):
+                    return 1
+            else:
+                return 0
+        else:
+            return 0
+    else:
+        return 0
+        
+def array(num):
+    ele = 3
+    primearr = set()
+    while(ele < num+1):
+        for i in range(2, int(math.sqrt(ele)) + 1):
+            if (ele % i == 0):
+                break
+        else:
+            primearr.add(ele)
+        ele+=1
+    return(primearr)
+ 
+def call(num):
+    if(num > 4):
+        x = math.sqrt(num)
+        s = str(x)
+        if(s[-2:] == '.0'):
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+ 
+     
+n = int(input())
+arr = list(map(int, input().split()))
+ 
+if len(arr) > 1:
+    m = int(math.sqrt(max(arr)))
+    primearr = array(m)
+    for i in arr:
+        if(i==4):
+            print('YES')
+        elif(i%2==0):
+            print('NO')
+        elif(call(i) and (int(math.sqrt(i)) in primearr)):
+            print("YES")
+        else:
+            print("NO")
+else:
+    if(callone(arr[0])):
+        print("YES")
+    else:
+        print("NO")
+        
 """//TLE
 import math
 
@@ -73,7 +200,52 @@ else:
         print("YES")
     else:
         print("NO")
- """        
+ """
+"""
+//array type method same TLE
+
+import math
+ 
+def array(num):
+    ele = 3
+    primearr = []
+    while(ele < num+1):
+        for i in range(2, int(math.sqrt(ele)) + 1):
+            if (ele % i == 0):
+                break
+        else:
+            primearr.append(ele)
+        ele+=1
+    return(primearr)
+    
+def call(num):
+    if(num > 4):
+        x = math.sqrt(num)
+        s = str(x)
+        if(s[-2:] == '.0'):
+            return 1
+        else:
+            return 0
+    else:
+        return 0
+        
+n = int(input())
+arr = list(map(int, input().split()))
+m = int(math.sqrt(max(arr)))
+# print(m)
+primearr = array(m)
+# print(primearr)
+ 
+for i in arr:
+    if(i==4):
+        print('YES')
+    elif(i%2==0):
+        print('NO')
+    elif(call(i) and (int(math.sqrt(i)) in primearr)):
+        print("YES")
+    else:
+        print("NO")
+"""
  """
  B. T-primes
 time limit per test2 seconds
